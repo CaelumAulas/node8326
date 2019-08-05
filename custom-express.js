d6462547c01a6b13a7a59afb75f3989f0ca871e4
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-08-05 17:11:50 -0300

    Aula01 Mysql, static e dotenv

--- custom-express.js
+++ custom-express.js
@@ -1,8 +1,13 @@
-const app = require('express')();
+const express = require('express');
 
 module.exports = function(){
+
+  const app = express();
     
   app.set('view engine', 'ejs');
+
+  app.use(express.static('./node_modules/bootstrap/dist'));
+  app.use(express.static('./node_modules/bootstrap/dist'));
   
   require('./routes/index')(app);
   require('./routes/produtos')(app);
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,5 +1,25 @@
+require('dotenv').config();
+
 module.exports = function(app){
   app.get('/produtos', function(request, response){
-    response.render('produtos/lista');
+    
+    const mysql = require('mysql');
+
+    const conexao = mysql.createConnection({
+      database: 'casadocodigo',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    })
+
+    conexao.query(`SELECT * FROM livros`, function(erro, listaLivros, fields){
+      //console.log(erro, listaLivros, fields);
+
+      response.render('produtos/lista', {listaLivros});
+      
+    })
+
+    conexao.end();
+
   })
 }
